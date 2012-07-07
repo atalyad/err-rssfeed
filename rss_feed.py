714027c1be491809c1d1f79390c1f80ac70e8558
Guillaume BINET <gbin@gootz.net>
2012-07-07 06:33:28 +0200

    ported to the new polling feature added debug logging for polling refactored details

--- rss_feed.py
+++ rss_feed.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import logging
 from threading import Timer
 from config import CHATROOM_PRESENCE
 from errbot.botplugin import BotPlugin
@@ -10,37 +11,33 @@ __author__ = 'atalyad'
 
 def get_item_date(rss_item):
     return datetime(rss_item.published_parsed.tm_year,
-                        rss_item.published_parsed.tm_mon,
-                        rss_item.published_parsed.tm_mday,
-                        rss_item.published_parsed.tm_hour,
-                        rss_item.published_parsed.tm_min,
-                        rss_item.published_parsed.tm_sec)
+                    rss_item.published_parsed.tm_mon,
+                    rss_item.published_parsed.tm_mday,
+                    rss_item.published_parsed.tm_hour,
+                    rss_item.published_parsed.tm_min,
+                    rss_item.published_parsed.tm_sec)
 
+DEFAULT_POLL_INTERVAL = 1800
 
 class RSSFeedPlugin(BotPlugin):
-
-    min_err_version = '1.3.0' # it needs the configuration feature and split_args_with=
+    min_err_version = '1.4.0' # it needs the new polling feature
 
     def get_configuration_template(self):
-        return {'POLL_INTERVAL' : 1800.0}
+        return {'POLL_INTERVAL': DEFAULT_POLL_INTERVAL}
 
     def configure(self, configuration):
         if configuration:
             if type(configuration) != dict:
-                super(RSSFeedPlugin, self).configure(None)
                 raise Exception('Wrong configuration type')
 
             if not configuration.has_key('POLL_INTERVAL'):
-                super(RSSFeedPlugin, self).configure(None)
                 raise Exception('Wrong configuration type, it should contain POLL_INTERVAL')
             if len(configuration) > 1:
-                super(RSSFeedPlugin, self).configure(None)
                 raise Exception('What else did you try to insert in my config ?')
             try:
-                float(configuration['POLL_INTERVAL'])
+                int(configuration['POLL_INTERVAL'])
             except:
-                super(RSSFeedPlugin, self).configure(None)
-                raise Exception('POLL_INTERVAL must be a float')
+                raise Exception('POLL_INTERVAL must be an integer')
         super(RSSFeedPlugin, self).configure(configuration)
 
 
@@ -69,43 +66,30 @@ class RSSFeedPlugin(BotPlugin):
 
 
     def send_news(self):
-
         """
         Go through RSS subscriptions, check if there's a new update and send it to the chat.
         """
+        logging.debug('Polling rss feeds')
         subscription_names = self.get_subscription_names()
         subscription_tss = self.get_subscriptions_last_ts()
         for name in subscription_names:
-            if subscription_tss[name] < datetime.now():
-                feed = parse(subscription_names[name])
-                if feed['entries']:
-                    item = feed['entries'][0]
-                    item_date = get_item_date(item)
-                    if item_date > subscription_tss[name]:
-                        subscription_tss[name] = item_date
-                        msg = '%s News from %s:\n%s' % (str(item_date), str(name), str(item.summary))
-                        self.send(CHATROOM_PRESENCE[0], msg)
-                        self.send(CHATROOM_PRESENCE[0], str(item.link))
+            feed = parse(subscription_names[name])
+            post_canary = False
+            if feed['entries']:
+                item = feed['entries'][0]
+                item_date = get_item_date(item)
+                if item_date > subscription_tss[name]:
+                    subscription_tss[name] = item_date
+                    self.send(CHATROOM_PRESENCE[0], '%s News from %s:\n%s' % (item_date, name, item.summary))
+                    self.send(CHATROOM_PRESENCE[0], str(item.link))
+                    self['subscriptions_last_ts'] = subscription_tss
+                    post_canary = True
+            if not post_canary:
+                logging.debug('No new rss item for %s' % name)
 
-                        self['subscriptions_last_ts'] = subscription_tss
-                        self.shelf.sync()
-
-        if not self.config:
-            poll_interval = 1800.0
-        else:
-            poll_interval = float(self.config.get('POLL_INTERVAL', 1800.0))
-
-
-        self.t = Timer(poll_interval, self.send_news)
-        self.t.setDaemon(True) # so it is not locking on exit
-        self.t.start()
-
-
-    def callback_connect(self):
-        if not self.config:
-            self.send(CHATROOM_PRESENCE[0], 'This plugin can be configured. Default polling interval is 1800.0 seconds. To modify run !config RSSFeedPlugin')
-        self.send_news()
-
+    def activate(self):
+        super(RSSFeedPlugin, self).activate()
+        self.start_poller(self.config['POLL_INTERVAL'] if self.config else DEFAULT_POLL_INTERVAL, self.send_news)
 
     @botcmd(split_args_with=' ')
     def rss_add(self, mess, args):
@@ -125,7 +109,6 @@ class RSSFeedPlugin(BotPlugin):
         if feed_name in self.get_subscription_names():
             return 'this feed already exists'
         self.add_subscription(feed_url, feed_name)
-        self.shelf.sync()
         return 'Feed %s added as %s' % (feed_url, feed_name)
 
 
@@ -140,7 +123,6 @@ class RSSFeedPlugin(BotPlugin):
         if feed_name not in self.get_subscription_names():
             return 'Sorry.. unknown feed...'
         self.remove_subscription(feed_name)
-        self.shelf.sync()
         return 'Feed %s was successfully removed.' % feed_name
 
 
@@ -156,11 +138,10 @@ class RSSFeedPlugin(BotPlugin):
         return ans
 
 
-    @botcmd(admin_only = True, split_args_with=' ')
+    @botcmd(admin_only=True, split_args_with=' ')
     def rss_clearfeeds(self, mess, args):
         """ WARNING : Deletes all existing feeds
         """
         self['subscription_names'] = {}
         self['subscriptions_last_ts'] = {}
-        self.shelf.sync()
         return 'all rss feeds were removed'
